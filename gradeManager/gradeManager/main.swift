ce8dae1ced27e9932d94175cf5cfa3ba64f96744
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-01-22 10:28:27 -0800

    add files

---
+++ gradeManager/gradeManager/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  gradeManager
+//
+//  Created by StudentAM on 1/22/24.
+//
+
+import Foundation
+import CSV
+
+do {
+    let stream = InputStream(fileAtPath: "/Users/studentam/Desktop/iOS practice/gradeManagment/grade.csv")
+    
+    
+} catch {
+    print("There is a error trying read the files. Check if the file path is correct")
+}
+
